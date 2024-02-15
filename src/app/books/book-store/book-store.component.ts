2d3f4575288ede4167ddcda56d00e2339deb8dd1
Hamid khan <hamiddikhan@gmail.com>
2024-02-15 11:48:20 +0500

    Search Book

--- src/app/books/book-store/book-store.component.ts
+++ src/app/books/book-store/book-store.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { Book, BooksByCategory } from '../../models/models';
-import { ApiService } from '../../shared/services/api.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { ApiService } from '../../shared/services/api.service';
 
 @Component({
   selector: 'book-store',
   templateUrl: './book-store.component.html',
-  styleUrl: './book-store.component.scss'
+  styleUrl: './book-store.component.scss',
 })
 export class BookStoreComponent {
   displayedColumns: string[] = [
@@ -40,34 +40,50 @@ export class BookStoreComponent {
   constructor(private apiService: ApiService, private snackBar: MatSnackBar) {
     apiService.getBooks().subscribe({
       next: (res: Book[]) => {
-        this.books=[];
-        res.forEach((b)=> this.books.push(b));
+        this.books = [];
+        res.forEach((b) => this.books.push(b));
 
-        this.booksToDisplay=[];
-        for(let book of this.books){
-          let categoryExists=false;
-          let categoryBook:BooksByCategory|null;
-          for(let bookToDisplay of this.booksToDisplay){
-            if(bookToDisplay.bookCategoryId==book.bookCategoryId){
-              categoryExists=true;
-              categoryBook=bookToDisplay;
-            }
-          } 
-          if(categoryExists){
-            categoryBook!.books.push(book);
-          }else{
-            this.booksToDisplay.push({
-              bookCategoryId: book.bookCategoryId,
-              category: book.bookCategory.category,
-              subCategory: book.bookCategory.subCategory,
-              books: [book],
-            });
-          }
-
-        }
+        this.updateList();
       },
     });
   }
+
+  updateList() {
+    this.booksToDisplay = [];
+    for (let book of this.books) {
+      let categoryExists = false;
+      let categoryBook: BooksByCategory | null;
+      for (let bookToDisplay of this.booksToDisplay) {
+        if (bookToDisplay.bookCategoryId == book.bookCategoryId) {
+          categoryExists = true;
+          categoryBook = bookToDisplay;
+        }
+      }
+
+      if (categoryExists) {
+        categoryBook!.books.push(book);
+      } else {
+        this.booksToDisplay.push({
+          bookCategoryId: book.bookCategoryId,
+          category: book.bookCategory.category,
+          subCategory: book.bookCategory.subCategory,
+          books: [book],
+        });
+      }
+    }
+  }
+
+  searchBooks(value: string) {
+    this.updateList();
+    value = value.toLowerCase();
+    this.booksToDisplay = this.booksToDisplay.filter((bookToDisplay) => {
+      bookToDisplay.books = bookToDisplay.books.filter((book) => {
+        return book.title.toLowerCase().includes(value);
+      });
+      return bookToDisplay.books.length > 0;
+    });
+  }
+
   getBookCount() {
     let count = 0;
     this.booksToDisplay.forEach((b) => (count += b.books.length));
