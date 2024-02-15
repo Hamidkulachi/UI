d110254b14a9eed9cf53a00f6aaf92004c7d7de6
Hamid khan <hamiddikhan@gmail.com>
2024-02-15 17:52:57 +0500

    Profile Component

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserOrdersComponent } from './user-orders/user-orders.component';
 import { SharedModule } from '../shared/shared.module';
+import { ProfileComponent } from './profile/profile.component';
 
 
 
 @NgModule({
   declarations: [
-    UserOrdersComponent
+    UserOrdersComponent,
+    ProfileComponent
   ],
   imports: [
     CommonModule,
