239bc152a7611bdfec3e283a4b08dc750950608e
Hamid khan <hamiddikhan@gmail.com>
2024-02-15 10:27:55 +0500

    View book feature

--- src/app/books/book-store/book-store.component.ts
+++ src/app/books/book-store/book-store.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Book, BooksByCategory } from '../../models/models';
+import { ApiService } from '../../shared/services/api.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'book-store',
@@ -6,5 +9,33 @@ import { Component } from '@angular/core';
   styleUrl: './book-store.component.scss'
 })
 export class BookStoreComponent {
+  displayedColumns: string[] = [
+    'id',
+    'title',
+    'author',
+    'price',
+    'available',
+    'order',
+  ];
+  books: Book[] = [];
+  booksToDisplay: BooksByCategory[] = [
+    {
+      bookCategoryId: 1,
+      category: 'C',
+      subCategory: 'S',
+      books: [
+        {
+          id: 1,
+          title: 'T',
+          author: 'A',
+          price: 100,
+          ordered: false,
+          bookCategoryId: 1,
+          bookCategory: { id: 1, category: '', subCategory: '' },
+        },
+      ],
+    },
+  ];
 
+  constructor(private apiService: ApiService, private snackBar: MatSnackBar) {}
 }
